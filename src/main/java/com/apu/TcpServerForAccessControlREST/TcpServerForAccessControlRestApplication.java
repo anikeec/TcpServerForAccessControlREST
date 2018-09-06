0c9ab3a04ec65a7adb06b29749e3dd5303b96424
apu <pasha_anik@ukr.net>
2018-09-06 19:21:32 +0300

    Init state

---
+++ src/main/java/com/apu/TcpServerForAccessControlREST/TcpServerForAccessControlRestApplication.java
@@ -0,0 +1,12 @@
+package com.apu.TcpServerForAccessControlREST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcpServerForAccessControlRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TcpServerForAccessControlRestApplication.class, args);
+	}
+}
---
+++ src/test/java/com/apu/TcpServerForAccessControlREST/TcpServerForAccessControlRestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.apu.TcpServerForAccessControlREST;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TcpServerForAccessControlRestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
